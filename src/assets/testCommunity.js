ef140d5b80ab4d9664e36942e2341c40502f092f
LeeeeDongHyun <106664658+LeeeeDongHyun@users.noreply.github.com>
2024-11-15 14:54:31 +0900

    Moke Data

---
+++ src/assets/testCommunity.js
@@ -0,0 +1,47 @@
+export const testCommunity = [
+    {
+        "id": 1,
+        "title": "혹시 보건계열로 일 하다가~~",
+        "content": "저는 내년에 8년차~~~",
+        "nickname": "또니이잉",
+        "good": 0,
+        "comment": 1,
+        "check" : 7
+    },
+    {
+        "id": 2,
+        "title": "간호사로 미국비자 대기중인데",
+        "content": "중환자실에 일을 하고싶은데~~~",
+        "nickname": "쪼이이잉",
+        "good": 2,
+        "comment": 3,
+        "check" :5
+    },
+    {
+        "id": 3,
+        "title": "다른계열에서 보건계열로 ~~~",
+        "content": "올해 3년차구요~~",
+        "nickname": "뽀이이잉",
+        "good": 3,
+        "comment": 2,
+        "check": 4
+    },
+    {
+        "id": 4,
+        "title": "한국에서 간호사~~~~~",
+        "content": "신입으로 들어갑니다~~~",
+        "nickname": "꼬이이잉",
+        "good": 1,
+        "comment": 1,
+        "check" :9
+    },
+    {
+        "id": 5,
+        "title": "5번째 스크롤 만들기",
+        "content": "스크롤만들기 content",
+        "nickname": "로이이잉",
+        "good": 100,
+        "comment": 100,
+        "check" : 100
+    }
+]
---
+++ src/assets/testMainCommunity.js
@@ -0,0 +1,57 @@
+export const testMainCommunity = [
+    {
+        "id": 1,
+        "title" : "ENERGY",
+        "description" : "에너지"
+    },
+    {
+        "id" : 2,
+        "title" : "MATERIALS",
+        "description" : "소재"
+    },
+    {
+        "id" : 3,
+        "title" : "INDUSTRIALS",
+        "description" : "산업재"
+    },
+    {
+        "id" : 4,
+        "title" : "CONSUMER_DISCRETIONARY",
+        "description" : "경기 관련 소비재"
+    },
+    {
+        "id": 5,
+        "title": "CONSUMER_STAPLES",
+        "description" : "필수 소비재"
+    },
+    {
+        "id": 6,
+        "title": "HEALTHCARE",
+        "description" : "건강관리"
+    },
+    {
+        "id": 7,
+        "title": "FINANCIAL",
+        "description" : "금융"
+    },
+    {
+        "id": 8,
+        "title": "IT",
+        "description" : "IT"
+    },
+    {
+        "id": 9,
+        "title": "COMMUNICATION_SERVICES",
+        "description" : "커뮤니케이션 서비스"
+    },
+    {
+        "id" : 10,
+        "title" : "UTILITIES",
+        "description" : "유틸리티"
+    },
+    {
+        "id" : 11,
+        "title" : "SECURITIES_FIRMS",
+        "description" : "증권사"
+    },
+]
