a9a03d9bd91c69f95335bf504b4ef60fad9f5b64
LeeeeDongHyun <106664658+LeeeeDongHyun@users.noreply.github.com>
2024-11-15 14:50:28 +0900

    Router 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,20 @@ import './css/sidebar.css'
 import SideBar from "./layouts/SideBar.jsx";
 import MainScreen from "./MainScreen.jsx";
 import Header from "./layouts/Header.jsx";
+import {Route, Routes} from "react-router-dom";
+import Community from "./Community.jsx";
 
 function App() {
     return (
         <div className="App w-full">
             <Header></Header>
             <SideBar></SideBar>
-            <MainScreen></MainScreen>
+
+            {/*<<MainScreen></MainScreen>> */}
+            <Routes>
+                <Route path="/" element={ <MainScreen/> }/>
+                <Route path="/community" element={ <Community/> }/>
+            </Routes>
         </div>
     );
 }
