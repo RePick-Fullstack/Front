edbaf72f35f851ad0183b890f2af89fa23d4c70b
LeeeeDongHyun <106664658+LeeeeDongHyun@users.noreply.github.com>
2024-11-18 03:33:33 +0900

    RecoilRoot 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,29 @@ import './css/sidebar.css'
 import SideBar from "./layouts/SideBar.jsx";
 import MainScreen from "./MainScreen.jsx";
 import Header from "./layouts/Header.jsx";
-import {Route, Routes} from "react-router-dom";
+import {Route, Router, Routes} from "react-router-dom";
 import Community from "./Community.jsx";
+import CommunityDetail from "./CommunityDetail.jsx";
+import {RecoilRoot} from "recoil";
+
+//import {testCommunity} from "./assets/testCommunity.js";
 
 function App() {
-    return (
-        <div className="App w-full">
-            <Header></Header>
-            <SideBar></SideBar>
 
-            {/*<<MainScreen></MainScreen>> */}
-            <Routes>
-                <Route path="/" element={ <MainScreen/> }/>
-                <Route path="/community" element={ <Community/> }/>
-            </Routes>
-        </div>
+    return (
+        <RecoilRoot>
+                <div className="App w-full">
+                    <Header></Header>
+                    <SideBar></SideBar>
+
+                    {/*<<MainScreen></MainScreen>> */}
+                    <Routes>
+                        <Route path="/" element={<MainScreen/>}/>
+                        <Route path="/community" element={<Community/>}/>
+                        <Route path="/community/:id" element={<CommunityDetail/>}/>
+                    </Routes>
+                </div>
+        </RecoilRoot>
     );
 }
 
