d6518c4c19a14afcd3861463cb63ff409972f7cd
LeeeeDongHyun <106664658+LeeeeDongHyun@users.noreply.github.com>
2024-11-22 17:58:49 +0900

    커뮤니티 전체보기 사이드바로 이동

--- src/layouts/SideBar.jsx
+++ src/layouts/SideBar.jsx
@@ -78,7 +78,7 @@ function SideBar() {
                                     d="M17 21H7a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h7l5 5v11a2 2 0 0 1-2 2m-5-10v6m-3-3h6"/></g></svg>
                             </span>
                             <div className={"cursor-pointer"} onClick={()=> {
-                                navigate("/community")
+                                navigate("/community?category=TOTAL")
                             }}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16">
                                     <g fill="currentColor">
