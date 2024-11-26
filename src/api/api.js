e39ef912722371bd5e93711ddd8e75695aac1df3
LeeeeDongHyun <106664658+LeeeeDongHyun@users.noreply.github.com>
2024-11-26 17:49:49 +0900

    baseURL 수정

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:9000/communities', // spring 서버 url
+    baseURL: 'http://localhost:9000/posts', // spring 서버 url
     timeout: 5000,
+    headers: {
+        'Content-Type': 'application/json', // 기본 Content-Type 설정
+    }
 });
 
 export const setAuthHeader = (token) =>{
