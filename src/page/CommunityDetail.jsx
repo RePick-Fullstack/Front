2bb4a9c8387e5b2cd613bfa633bb9ac3b7c47f0c
LeeeeDongHyun <106664658+LeeeeDongHyun@users.noreply.github.com>
2024-11-29 16:28:07 +0900

    id 별 게시글 받아오는 중

--- src/page/CommunityDetail.jsx
+++ src/page/CommunityDetail.jsx
@@ -5,16 +5,20 @@ import {useEffect, useState} from "react";
 
 
 function CommunityDetail() {
+
     const [post, setPost] = useState([]);
-    let {id} = useParams();
+
+    const {id} = useParams();
+
     useEffect(() => {
         handlePost()
     }, []);
 
     const handlePost = async () => {
-        const newPost = await getPostById(id)
-        setPost(newPost)
-        console.log(newPost)
+        console.log(" 클릭한 게시글 id " + id)
+        const fetchedPost = await getPostById(id);
+        console.log("fetchedPost 찍음 " + JSON.stringify(fetchedPost, null, 2))
+        setPost(fetchedPost)
     }
 
     return (
@@ -23,13 +27,22 @@ function CommunityDetail() {
                 <div className={"bg-gray-300 rounded-xl font-bold p-10"} style={{margin: "50px 100px -50px 100px"}}>
                     {/*<span> {category[0].title}</span> /!* 해당하는 category 뜨게 할 예정 *!/*/}
                     <div className={"border-amber-100 mb-5"}>
+                        {/*  posts.map((post, index) => (*/}
+                        {/*    <div className={"bg-white mt-5"} key={index}> 제목 : {post.title}*/}
+                        {/*        <p className={"mb-5 text-right"}>조회 {post.viewCount}</p>*/}
+                        {/*        <p>게시글 id : {post.id}</p>*/}
+                        {/*        <div>내용 : {post.content}</div>*/}
+                        {/*        /!*<div>{post.nickname}</div>*!/*/}
+                        {/*    </div>*/}
+                        {/*))}*/}
                         <div className={"bg-white mt-5"}> 제목 : {post.title}
                             <p className={"mb-5 text-right"}>조회 {post.viewCount}</p>
+                            <p>게시글 id : {post.id}</p>
                             <div>내용 : {post.content}</div>
-                            {/*<div>{post.nickname}</div>*/}
                         </div>
-                    </div>
-                    <div className={"bg-amber-800 rounded-l p-5"}>댓글 0
+
+                        </div>
+                        <div className={"bg-amber-800 rounded-l p-5"}>댓글 0
                         <div className={"bg-yellow-500 rounded-l p-5"}>따뜻한 댓글을 남겨주세요 :)
                             <div><input placeholder={"댓글을 입력해주세요 :)"} className={"bg-white rounded-l"}></input></div>
                         </div>
