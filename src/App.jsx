672a60733546ddb83a581a8cab338cb4b8b32c45
choi14123 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-12-02 14:24:46 +0900

    refactor: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,9 @@ import MainScreen from "./page/MainScreen.jsx";
 import Header from "./layout/Header.jsx";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Community from "./page/Community.jsx";
-<<<<<<< HEAD
-import PostDetail from "./page/PostDetail.jsx";
+
 import {RecoilRoot} from "recoil";
-=======
-import CommunityDetail from "./page/CommunityDetail.jsx";
-import { RecoilRoot } from "recoil";
->>>>>>> 2d62035 (feat: 어드민 페이지 개발)
+
 import MyPage from "./page/MyPage.jsx";
 import ReportPage from "./page/ReportPage.jsx";
 import MainKakaoSignUp from "./page/mainuser/MainKakaoSignUp.jsx";
@@ -25,6 +21,7 @@ import ChatBot from "./page/ChatBot.jsx";
 import AdminMain from "./page/admin/AdminMain.jsx";
 import SuperAdmin from "./page/admin/SuperAdmin.jsx";
 import AdminUser from "./page/admin/AdminUser.jsx";
+import PostDetail from "./page/PostDetail.jsx";
 
 function App() {
     const location = useLocation(); // 현재 경로 가져오기
@@ -33,25 +30,6 @@ function App() {
     const hiddenSidebarPaths = ['/admin/super','/admin/main', '/admin/user'];
     const shouldHideSidebar = hiddenSidebarPaths.includes(location.pathname);
 
-<<<<<<< HEAD
-            <Routes>
-                <Route path="/*" element={<MainKakaoSignUp/>}/>
-                <Route path="/" element={<MainScreen/>}/>
-                <Route path="/community" element={<Community/>}/>
-                <Route path="/createpost" element={<CreatePost/>}/>
-                <Route path="/posts/:id" element={<PostDetail/>}/>
-                <Route path="/reportpage" element={<ReportPage/>}/>
-                <Route path="/mypage" element={<MyPage/>}/>
-                <Route path="/chatroom/" element={<ChatRoomList/>}/>
-                <Route path="/chatroom/:id" element={<ChatRoom/>}/>
-                <Route path="/chatbot" element={<ChatBot/>}/>
-                <Route path="/tosspayment" element={<PaymentCheckoutPage/>}/>
-                <Route path="/tosspayment/success" element={<PaymentSuccessPage/>}/>
-                <Route path="/tosspayment/fail" element={<PaymentFailPage/>}/>
-            </Routes>
-        </div>
-    </RecoilRoot>);
-=======
     return (
         <RecoilRoot>
             <div className="App w-full">
@@ -66,7 +44,7 @@ function App() {
                     <Route path="/" element={<MainScreen />} />
                     <Route path="/community" element={<Community />} />
                     <Route path="/createpost" element={<CreatePost />} />
-                    <Route path="/posts/:id" element={<CommunityDetail />} />
+                    <Route path="/posts/:id" element={<PostDetail />} />
                     <Route path="/reportpage" element={<ReportPage />} />
                     <Route path="/mypage" element={<MyPage />} />
                     <Route path="/chatroom/" element={<ChatRoomList />} />
@@ -79,7 +57,6 @@ function App() {
             </div>
         </RecoilRoot>
     );
->>>>>>> 2d62035 (feat: 어드민 페이지 개발)
 }
 
 export default App;
