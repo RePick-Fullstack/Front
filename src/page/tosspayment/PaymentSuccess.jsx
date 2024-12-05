2d30ba4c181164249e5f294db63e40a22ff93c51
JoGyoungJun <yoop80075@gmail.com>
2024-12-05 16:42:53 +0900

    메인카프카 추가

--- src/page/tosspayment/PaymentSuccess.jsx
+++ src/page/tosspayment/PaymentSuccess.jsx
@@ -21,6 +21,7 @@ export function PaymentSuccessPage() {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
+          "Authorization": `Bearer ${localStorage.getItem("accessToken")}`
         },
         body: JSON.stringify(requestData),
       });
