47b2d146171966a1614b96552d83d49af1d83b39
choi14123 <choichanghwan@choechanghwan-ui-MacBookPro.local>
2024-12-05 10:44:26 +0900

    refactor: 어드민 페이지 분리

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './css/sidebar.css';
 import SideBar from "./layout/SideBar.jsx";
 import MainScreen from "./page/MainScreen.jsx";
 import Header from "./layout/Header.jsx";
-import { Route, Routes, useLocation, Navigate } from "react-router-dom"; // Navigate 추가
+import { Route, Routes, Navigate } from "react-router-dom"; // Navigate 추가
 import Community from "./page/Community.jsx";
 import { RecoilRoot } from "recoil";
 import MyPage from "./page/MyPage.jsx";
@@ -16,31 +16,19 @@ import { PaymentCheckoutPage } from "./page/tosspayment/PaymentCheckout.jsx";
 import { PaymentSuccessPage } from "./page/tosspayment/PaymentSuccess.jsx";
 import { PaymentFailPage } from "./page/tosspayment/PaymentFail.jsx";
 import ChatBot from "./page/ChatBot.jsx";
-import AdminMain from "./page/admin/AdminMain.jsx";
-import AdminSuper from "./page/admin/AdminSuper.jsx";
-import AdminUser from "./page/admin/AdminUser.jsx";
 import PostDetail from "./page/PostDetail.jsx";
-import AdminSignIn from "./page/admin/AdminSignIn.jsx";
 
 function App() {
-    const location = useLocation(); // 현재 경로 가져오기
 
-    // 특정 경로에서 사이드바 숨기기
-    const hiddenSidebarPaths = ['/admin/login-xXx', '/admin/super', '/admin/main', '/admin/user'];
-    const shouldHideSidebar = hiddenSidebarPaths.includes(location.pathname);
 
     return (
         <RecoilRoot>
             <div className="App w-full">
-                {!shouldHideSidebar && <Header />} {/* 사이드바 조건부 렌더링 */}
-                {!shouldHideSidebar && <SideBar />} {/* 사이드바 조건부 렌더링 */}
+                {<Header />}
+                {<SideBar />}
 
                 <Routes>
                     {/* 명시된 라우트 */}
-                    <Route path="/admin/login-xXx" element={<AdminSignIn />} />
-                    <Route path="/admin/super" element={<AdminSuper />} />
-                    <Route path="/admin/main" element={<AdminMain />} />
-                    <Route path="/admin/user" element={<AdminUser />} />
                     <Route path="/complete-profile" element={<MainKakaoSignUp />} />
                     <Route path="/" element={<MainScreen />} />
                     <Route path="/community" element={<Community />} />
--- src/api/api.js
+++ src/api/api.js
@@ -23,19 +23,6 @@ export const usersApi = axios.create({
     }
 });
 
-usersApi.interceptors.request.use((config) => {
-    const accessToken = localStorage.getItem("adminAccessToken");
-    const refreshToken = localStorage.getItem("adminRefreshToken");
-
-    // 특정 URL에만 refreshToken을 사용
-    if (config.url === "/admin/refresh-token" && refreshToken) {
-        config.headers.Authorization = `Bearer ${refreshToken}`;
-    } else if (accessToken) {
-        config.headers.Authorization = `Bearer ${accessToken}`;
-    }
-    return config;
-});
-
 export const eksApi = axios.create({
     baseURL: 'http://k8s-repick-59dd78f5ea-980331128.us-east-1.elb.amazonaws.com/api/v1', // spring 서버 url
 });
