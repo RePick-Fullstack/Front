95fbb0a75e2b65e76ec8332698bfe89c0ae9a361
LeeeeDongHyun <106664658+LeeeeDongHyun@users.noreply.github.com>
2024-12-09 17:49:42 +0900

    게시글 수정 페이지

---
+++ src/page/ModifyPost.jsx
@@ -0,0 +1,10 @@
+function ModifyPost(){
+    return (
+        <>
+        <div className={"m-auto relative"}>
+            게시글 수정 ㅇㅇ
+        </div>
+        </>
+    )
+}
+export default ModifyPost;
