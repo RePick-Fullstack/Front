6bbe06000f794c8910f27dd0a4adf603087fecf4
LeeeeDongHyun <106664658+LeeeeDongHyun@users.noreply.github.com>
2024-12-09 11:22:12 +0900

    SideBar 수정

--- src/layout/SideBar.jsx
+++ src/layout/SideBar.jsx
@@ -7,6 +7,7 @@ import SideBarClose from "../assets/sideBarClose.svg"
 import Home from "../assets/home.svg"
 import ReportDownload from "../assets/reportDownload.svg"
 import Community from "../assets/community.svg"
+import ChatHistory from "./ChatHistory.jsx";
 
 function SideBar() {
     let navigate = useNavigate();
@@ -38,22 +39,28 @@ function SideBar() {
                     <div className={"flex justify-between items-center mb-9 w-3/4 ml-1"}>
                         <button onClick={() => {  //홈 버튼
                             navigate('/');
-                            location.reload();
+                            setMenuOpen(false);
                         }}>
                             <img src={Home} alt="Home Logo"/>
                         </button>
                         <button onClick={() => { //사이드바 열기
                             setMenuOpen(!menuOpen)
                         }}>{menuOpen ?
-                             <img src={SideBarClose} alt="SideBar Logo"/> : ""}
+                            <img src={SideBarClose} alt="SideBar Logo"/> : ""}
                         </button>
                     </div>
                     {menuOpen ? (
                         <>
-                            <h4 className={"cursor-pointer hover:underline font-semibold text-white"} onClick={() => { //사이드바 열렸을때
+                            <h4 className={"cursor-pointer hover:underline font-semibold text-white m-5"}
+                                onClick={() => {
+                                    handleNavigation("/ChatBot")
+                                }}>새 질문</h4>
+                            <h4 className={"cursor-pointer hover:underline font-semibold text-white m-5"} onClick={() => { //사이드바 열렸을때
                                 handleNavigation("/myPage")
                             }}>마이 페이지</h4>
-                            <div className="sideBarHome"><a href="#new2">New Content 2</a></div>
+                            <h3 className={"bg-white h-2/4 w-3/4 p-2 rounded-xl"}>
+                                <ChatHistory/> {/* 여기다가 chatHIstory map 써서 나오게 하면 될듯 */}
+                            </h3>
                         </>
                     ) : (
                         <>
