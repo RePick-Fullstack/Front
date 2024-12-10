46b22dc8d43cb8d7cb5d23edefb50ea3c280203f
LeeeeDongHyun <106664658+LeeeeDongHyun@users.noreply.github.com>
2024-12-10 17:51:00 +0900

    페이지 이름 변경

---
+++ src/page/EditPost.jsx
@@ -0,0 +1,14 @@
+function EditPost(){
+    return (
+        <>
+            <div className={"text-center"}>
+                게시글 수정 ㅇㅇ<br/>
+                화면 만들고 <br/>
+                만든 작성자의 userId와<br/>
+                수정페이지 들어오는 userId가 똑같아야<br/>
+                화면이 나오게 만들거임 ㅇㅇ<br/>
+            </div>
+        </>
+    )
+}
+export default EditPost;
