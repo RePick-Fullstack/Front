075a1795af33e62d95eb0d00f3d3e4759fc673fe
LeeeeDongHyun <106664658+LeeeeDongHyun@users.noreply.github.com>
2024-12-12 13:18:20 +0900

    css 수정

--- src/page/MainScreen.jsx
+++ src/page/MainScreen.jsx
@@ -151,22 +151,22 @@ function MainScreen() {
     };
 
     return (
-        <div className={"ml-[150px]"} style={{maxWidth: `calc(100% - 50px)`}}>
+        <div className={"ml-[10px] h-[100vh]"} style={{maxWidth: `calc(100% - 50px)`}}>
             <div className="main_container justify-center">
                 <div className="left_container">
                     <div className="report_header">
                         {enterDelay || <div className="hotReport">
-                            <p className={"font-bold text-2xl ml-11 p-3"}>종목분석 레포트</p>
+                            <p className={"font-bold text-xl ml-11 p-3"}>종목분석 레포트</p>
                             <ul className={"font-extrabold"}>
-                                <hr className={"border-black border-[1px]"}/>
-                                <li className={"grid grid-cols-4 sm:grid-cols-4 md:grid-cols-4 gap-4 px-4 py-2"}>
+                                <hr className={"border-whitesmoke border-[0.5px]"}/>
+                                <li className={"grid grid-cols-4 sm:grid-cols-4 md:grid-cols-4 font-black text-[13px] gap-4 px-4 py-2"}>
                                     <span className="text-left">기업</span>
                                     <span className="text-left">제목</span>
                                     <span className="text-left ml-3">증권사</span>
                                     <span className="text-left">발행 일자</span>
                                 </li>
                             </ul>
-                            <hr className={"border-black border-[1px]"}/>
+                            <hr className={"border-whitesmoke border-[1px]"}/>
                             <div className="report_scroll">
                                 <ul> {/*     종목분석 레포트     */}
                                     {reports.map((report, index) =>
@@ -187,17 +187,17 @@ function MainScreen() {
                         </div>}
                     </div>
                     {enterDelay || <div className="industryReport">
-                        <p className={"font-bold text-2xl ml-11 p-3"}>산업분석 레포트</p>
+                        <p className={"font-bold text-xl ml-11 p-3"}>산업분석 레포트</p>
                         <ul className={"font-extrabold"}>
-                            <hr className={"border-black border-[1px]"}/>
-                            <li className={"grid grid-cols-4 sm:grid-cols-2 md:grid-cols-4 gap-4 px-4 py-2"}>
+                            <hr className={"border-whitesmoke border-[1px]"}/>
+                            <li className={"grid grid-cols-4 sm:grid-cols-2 md:grid-cols-4 text-[13px] gap-4 px-4 py-2"}>
                                 <span className="text-left">기업</span>
                                 <span className="text-left">제목</span>
-                                <span className="text-center">증권사</span>
+                                <span className="text-left ml-3">증권사</span>
                                 <span className="text-right">발행 일자</span>
                             </li>
                         </ul>
-                        <hr className={"border-black border-[1px]"}/>
+                        <hr className={"border-whitesmoke border-[1px]"}/>
                         <div className="industry_scroll">
                             <ul>       {/*    여기에 산업분석 레포트 나오는거 넣기  */}
                                 <li></li>
@@ -209,13 +209,12 @@ function MainScreen() {
 
                     <div className="newsCrawling">     {/*뉴스 컴포넌트*/}
                         <div>
-                            <p className={"news_title font-bold text-2xl p-3"}>뉴스</p>
-                            <hr className={"border-black border-[1px]"}/>
+                            <p className={"news_title font-bold text-xl p-3 ml-2"}>뉴스</p>
                             {news.map((item, index) => (
-                                <div className={"hover:bg-gray-200 rounded-xl p-2"} key={index}>
+                                <div className={"w-[20vw] hover:bg-gray-200 text-[12.5px] font-semibold rounded-xl p-3 ml-4"} key={index}>
                                     <a href={item.url} target="_blank" rel="noopener noreferrer"
                                        style={{color: `black`}}>{item.title}</a>
-                                    <hr/>
+
                                 </div>
                             ))}
                         </div>
@@ -223,9 +222,9 @@ function MainScreen() {
                     <div className={"communityOrder"}>   {/*  커뮤니티 컴포넌트   */}
                     </div>
                     <div className="community">
-                        <span className={"font-bold text-2xl"}>커뮤니티</span>
+                        <span className={"font-bold text-lg"}>커뮤니티</span>
                         {/* 페이지 시작은 조회순으로 시작하고 인기순 누르면 조회 or 좋아요로 orderBy*/}
-                        <div className={"float-right active:border-black"}>
+                        <div className={"float-right active:border-black text-[11px]"}>
                             <button className={"mr-5"} onClick={() => {
                                 console.log("조회순 누름")
                             }}>조회순
@@ -263,9 +262,9 @@ function MainScreen() {
                 </div>
                 <div className={"flex justify-center"}>
                     <div className="inputContainer flex w-full justify-center">
-                        <div className={"relative w-full flex"} style={{maxWidth: `744px`}}>
+                        <div className={"relative w-full flex"} style={{maxWidth: `900px`}}>
                             <input
-                                className="chatInput rounded-2xl border border-black flex"
+                                className="chatInput rounded-2xl flex"
                                 placeholder="어떤 레포트가 궁금하신가요?"
                                 value={inputValue}
                                 onChange={(e) => setInputValue(e.target.value)}
@@ -277,7 +276,7 @@ function MainScreen() {
                                 <button className="inputButton" onClick={handleSendRequest}
                                         disabled={enterDelay}> {/* enterDelay 상태에 따라 버튼 비활성화 */}
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                         className="input-svg">
+                                         height="25px" width="25px">
                                         <path
                                             d="M1.94607 9.31543C1.42353 9.14125 1.4194 8.86022 1.95682 8.68108L21.043 2.31901C21.5715 2.14285 21.8746 2.43866 21.7265 2.95694L16.2733 22.0432C16.1223 22.5716 15.8177 22.59 15.5944 22.0876L11.9999 14L17.9999 6.00005L9.99992 12L1.94607 9.31543Z"
                                         ></path>
@@ -287,6 +286,7 @@ function MainScreen() {
                         </div>
                     </div>
                 </div>
+                <p className={"relative justify-center w-full flex text-[10px]"}>AI는 실수할 수 있습니다. 원문을 꼭 확인하시길 바라며, 투자는 개인의 책임입니다.</p>
             </div>
         </div>
     );
