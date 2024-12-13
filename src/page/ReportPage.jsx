322690cf896421e33609eeb9a24ed0445d14cb37
LeeeeDongHyun <106664658+LeeeeDongHyun@users.noreply.github.com>
2024-12-14 04:34:09 +0900

    내용 및 css 수정

--- src/page/ReportPage.jsx
+++ src/page/ReportPage.jsx
@@ -41,23 +41,19 @@ function ReportPage() {
                     </div>
                     <div className="right_report_container">
                         <ul className={"font-black"}>
-                            <li className={"grid grid-cols-[2.5fr_4fr_3fr_3fr_2fr] px-4 py-2"}>
-                                <span className="text-left">기업</span>
-                                <span className="text-left">제목</span>
-                                <span className="text-left">증권사</span>
-                                <span className="text-center mr-16">발행 일자</span>
+                            <li className={"grid grid-cols-[5fr_4fr_1fr] px-4 py-2"}>
+                                <span className="text-left">레포트 제목</span>
+                                <span className="text-left">발행 일자</span>
                                 <span className="text-right">다운로드</span>
                             </li>
                             {reports.map((report, index) =>
                                     <li key={index}>
                                         <div
-                                            className={"grid grid-cols-[3fr_4fr_4fr_2fr_1fr] px-4 py-2 border-t border-gray-300"}>
-                                            <span className={"text-left"}>{`${report.company_name}`}</span>
-                                            <span className={"text-center"}><a className={"ml-5"}
-                                                                               href={report.pdf_link}>{report.sector_name}</a></span>
-                                            <span className={"text-right"}>{report.report_date}</span>
-                                            <span className={"flex flex-col items-center"}>
-                                <a className={"ml-20"}
+                                            className={"grid grid-cols-[5fr_4fr_1fr] px-4 py-2 border-t border-gray-300"}>
+                                            <span className={"text-left"}>{report.report_title}</span>
+                                            <span className={"text-left"}>{report.report_date}</span>
+                                            <span className={"flex-col flex"}>
+                                <a className={"ml-14"}
                                    href={report.pdf_link}>
                                     <img src={Pdf} alt="Pdf Logo"/>
                             </a>
