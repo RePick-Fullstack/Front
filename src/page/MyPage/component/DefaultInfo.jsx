1388c84c3e5846e9be89d144e14a0257030f5ffe
LeeeeDongHyun <106664658+LeeeeDongHyun@users.noreply.github.com>
2024-12-15 22:07:06 +0900

    margin top 추가

--- src/page/MyPage/component/DefaultInfo.jsx
+++ src/page/MyPage/component/DefaultInfo.jsx
@@ -91,11 +91,11 @@ export const DefaultInfo = () => {
     return (
         <div>
             <div className={"flex flex-row mb-5"}>
-                <div className={"font-bold text-2xl mb-2 "}>기본정보</div>
+                <div className={"font-bold text-2xl mb-2 mt-5 "}>기본정보</div>
                 <div>
                     <button
                         onClick={handleEditClick}
-                        className="border px-3 text-white py-2 rounded bg-gray-500 ml-3"
+                        className="border px-3 text-white py-2 rounded bg-gray-500 ml-3 mt-2"
                     >
                         수정
                     </button>
@@ -103,7 +103,7 @@ export const DefaultInfo = () => {
                 <div>
                     <button
                         onClick={handleDeleteAccount}
-                        className="bg-[#2c3e50] text-white px-3 py-2 rounded ml-3"
+                        className="bg-[#2c3e50] text-white px-3 py-2 rounded ml-3 mt-2"
                     >
                         회원 탈퇴
                     </button>
