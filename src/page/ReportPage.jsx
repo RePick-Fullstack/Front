c418a714155d27ffba3123c52b887818ae208869
LeeeeDongHyun <106664658+LeeeeDongHyun@users.noreply.github.com>
2024-12-15 19:14:24 +0900

    기업 종목 useState로 변경

--- src/page/ReportPage.jsx
+++ src/page/ReportPage.jsx
@@ -10,12 +10,14 @@ import Pdf from "../assets/pdf.svg"
 function ReportPage() {
     const [reports, setReports] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [companyName, setCompanyName] = useState("");
 
     const handleCompanyReports = async () => {
         setLoading(true);
         const {data: {content: getReports}} = await eksApi.get("/reports/company",
             {params: {page: 0, size: 10}}).catch(err => console.log(err));
         setReports(getReports);
+        console.log(JSON.stringify(getReports,null,2));
         setLoading(false);
     };
 
@@ -40,30 +42,34 @@ function ReportPage() {
                 <div className={"selected_report"}>
                     <div className="select_report_kind hover:cursor-pointer items-center">
                         <div tabIndex="0"
-                             className={" w-[190px] h-[52px] focus:bg-[rgba(2,32,71,0.05)] text-center rounded-xl flex items-center justify-center"}
+                             className={" w-[190px] h-[52px] focus:bg-[rgba(2,32,71,0.15)] text-center rounded-xl flex items-center justify-center"}
                             onClick={handleCompanyReports}
                         >
-                            <p>종목분석 레포트</p>
+                            <p onClick={()=> { setCompanyName("기업"); }} >종목분석 레포트</p>
                         </div>
                         <div tabIndex="0"
-                             className={"px-18 py12 w-[190px] h-[52px] focus:bg-[rgba(2,32,71,0.05)] text-center rounded-xl flex items-center justify-center"}
+                             className={" w-[190px] h-[52px] focus:bg-[rgba(2,32,71,0.15)] text-center rounded-xl flex items-center justify-center"}
                              onClick={handleIndustryReports}
                         >
-                            <p>산업분석 레포트</p>
+                            <p onClick={()=> { setCompanyName("종목"); }} >산업분석 레포트</p>
                         </div>
                     </div>
                     <div className="right_report_container">
                         <ul className={"font-black"}>
-                            <li className={"grid grid-cols-[5fr_4fr_1fr] px-4 py-2"}>
+                            <li className={"grid grid-cols-[1fr_4fr_1fr_2fr_1fr] px-4 py-2"}>
+                                <span className="text-left">{companyName}</span>
                                 <span className="text-left">레포트 제목</span>
+                                <span className="text-left">증권사</span>
                                 <span className="text-left">발행 일자</span>
                                 <span className="text-right">다운로드</span>
                             </li>
                             {!loading && reports.map((report, index) =>
                                     <li key={index}>
                                         <div
-                                            className={"grid grid-cols-[5fr_4fr_1fr] px-4 py-2 border-t border-gray-300"}>
+                                            className={"grid grid-cols-[1fr_4fr_1fr_2fr_1fr] px-4 py-2 border-t border-gray-300"}>
+                                            <span className={"text-left"}>{report.company_name}</span>
                                             <span className={"text-left"}>{report.report_title}</span>
+                                            <span className={"text-left"}>{report.securities_firm}</span>
                                             <span className={"text-left"}>{report.report_date}</span>
                                             <span className={"flex-col flex"}>
                                 <a className={"ml-14"}
