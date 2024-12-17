63dc280ccb35e3cbebf52fda091949843393d5d5
JoGyoungJun <yoop80075@gmail.com>
2024-12-17 21:30:25 +0900

    결제 완료 핸들링

--- src/page/tosspayment/PaymentSuccess.jsx
+++ src/page/tosspayment/PaymentSuccess.jsx
@@ -46,37 +46,40 @@ if(searchParams.get("paymentKey")) {
   }, [searchParams]);
 
   return (
-    <div className="toss_font">
-      <div className="box_section" style={{ width: "600px" }}>
-        <div className="flex justify-center">
-          <img width="100px" src="https://static.toss.im/illusts/check-blue-spot-ending-frame.png" />
-        </div>
-        <h2 className={"text-2xl font-bold my-5"}>결제를 완료했어요</h2>
-        <div className="p-grid typography--p" style={{ marginTop: "50px" }}>
-          <div className="p-grid-col text--left">
-            <b>결제금액</b>
+      <div className="w-full ml-[50px] flex items-center"
+           style={{minHeight: `calc(100vh - 78px)`, maxWidth: `calc(100% - 50px)`}}>
+        <div className="flex px-2 w-full">
+          <div className="flex flex-col items-center justify-center h-full gap-5 w-full">
+            <div
+                className="border-[3px] border-solid border-[#f5f5f5] rounded-[25px] w-[800px] h-20 shadow-[2px_2px_2px_2px_#f5f5f5] text-[30px] font-bold flex items-center justify-center">
+              결제를 완료했어요
+            </div>
+            <div
+                className="border-[3px] border-solid border-[#f5f5f5] rounded-[25px] w-[800px] h-[278px] shadow-[2px_2px_2px_2px_#f5f5f5] p-5 text-2xl font-bold flex flex-col px-48">
+              <div className="flex justify-center">
+                <img width="100px" src="https://static.toss.im/illusts/check-blue-spot-ending-frame.png"/>
+              </div>
+              <div className="p-grid typography--p" style={{marginTop: "50px"}}>
+                <div className="p-grid-col text--left">
+                  <b>결제금액</b>
+                </div>
+                <div className="p-grid-col text--right" id="amount">
+                  {`${Number(searchParams.get("amount")).toLocaleString()}원`}
+                </div>
+              </div>
+              <div className="p-grid typography--p" style={{marginTop: "10px"}}>
+                <div className="p-grid-col text--left">
+                  <b>주문번호</b>
+                </div>
+                <div className="p-grid-col text--right" id="orderId">
+                  {`${searchParams.get("orderId")}`}
+                </div>
+              </div>
+              <div className="p-grid-col">
+              </div>
+            </div>
           </div>
-          <div className="p-grid-col text--right" id="amount">
-            {`${Number(searchParams.get("amount")).toLocaleString()}원`}
-          </div>
-        </div>
-        <div className="p-grid typography--p" style={{ marginTop: "10px" }}>
-          <div className="p-grid-col text--left">
-            <b>주문번호</b>
-          </div>
-          <div className="p-grid-col text--right" id="orderId">
-            {`${searchParams.get("orderId")}`}
-          </div>
-        </div>
-        <div className="p-grid-col">
         </div>
       </div>
-      {/*<div className="box_section" style={{ width: "600px", textAlign: "left" }}>*/}
-      {/*  <b>Response Data :</b>*/}
-      {/*  <div id="response" style={{ whiteSpace: "initial" }}>*/}
-      {/*    {responseData && <pre>{JSON.stringify(responseData, null, 4)}</pre>}*/}
-      {/*  </div>*/}
-      {/*</div>*/}
-    </div>
   );
 }
